8f15cccddd0c000e02cbec6cba41c7aeaa529937
mr.domala <rahuldomala12@gmail.com>
2021-12-07 14:26:44 +0530

    Commit

---
+++ Stone/src/test/java/Mile/Stone/HomePage.java
@@ -0,0 +1,9 @@
+package Mile.Stone;
+import org.openqa.selenium.WebDriver;
+public class HomePage {
+	WebDriver driver;
+	public HomePage(WebDriver driver) {   //Mapping driver to the driver which is currently working on using constructor 
+		this.driver = driver;
+	}
+	
+}
